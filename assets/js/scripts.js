fc09698a3c729a0f088fe25d2f8ccb16069eb1dc
ByteDevMaven <vrsanchesu@gmail.com>
2025-01-30 22:57:17 -0600

    - v1 of CSS ui design - working tabs - working posts using json - theme button - v1 of css responsive - new folder structure - public items, sitemap, robots, webmanifest - wip links.html - New Icon

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,29 +1,16 @@
 import newPost from "./post.js";
 
-const posts = [
-    {
-        avatar: './assets/images/profile.jpg',
-        name: 'Carlos Sanchez',
-        date: '29/1/2025',
-        title: 'Hello World!',
-        text: 'This is my personal portfolio site where I post my job experience, and what projects I have and are working on. If you are interested, you can contact me or leave your thoughts on the Guestbook.',
-        image: './assets/images/post.webp'
-    },
-    {
-        avatar: './assets/images/profile.jpg',
-        name: 'Carlos Sanchez',
-        date: '6/2/2024',
-        title: 'First Web App',
-        text: 'My first web app was using the Vue js framework using a google sheet\'s app script by Google as a basic API while working for Fourth Enterprises in customer service. You can check it <a class="links" href="https://github.com/ByteDevMaven/Vue-Tickets">/here</a>.',
-        image: './assets/images/vue.webp'
-    }
-]
+async function fetchPosts() {
+    const response = await fetch('./data/posts.json'); // Fetch the JSON file
+    const posts = await response.json(); // Convert response to JSON
+    addPost(posts);
+}
 
-function addPost() {
+function addPost(posts) {
     const addSelectedPosts = document.getElementById("post__container");
-    posts.forEach(shoe => {
-        addSelectedPosts.appendChild(newPost(shoe));
+    posts.forEach(post => {
+        addSelectedPosts.appendChild(newPost(post));
     })
 }
 
-document.addEventListener("DOMContentLoaded", addPost);
+document.addEventListener("DOMContentLoaded", fetchPosts);
